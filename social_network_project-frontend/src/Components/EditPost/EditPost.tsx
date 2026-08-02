370912ca0c8681c1c728b1af8b7d71fc71126d06
Ancie13 <vladyslavkhomenko13@gmail.com>
2026-08-02 06:22:02 +0300

    ts fix 7 and fallback

--- social_network_project-frontend/src/Components/EditPost/EditPost.tsx
+++ social_network_project-frontend/src/Components/EditPost/EditPost.tsx
@@ -204,7 +204,7 @@ export default function EditPost({
                 {imageUrlLocal && (
                     <div className="previewWrapper">
                         <img
-                            src={imageUrl}
+                            src={imageUrl!}
                             className="previewImage"
                         />
 
